93f41cb5ea03865fe553b59f4582904643f3f99d
samuelconlon <conlonsp@yahoo.com>
2022-07-19 14:30:10 -0400

    commit tree 8c5a26fc8426a994ae5adc26a371d8fbe4e730ab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Add your code here
+//const formData = {
+  //name: '',
+  //email: ''
+//}
+
+//const configObj = {
+  //method: 'POST',
+  //headers: {
+    //'Content-Type': 'application/json',
+    //'Accept': 'application/json',
+  //},
+  //body: JSON.stringify(formData),
+//}
+
+  function submitData(name, email) {
+    const formData = {
+      name: `${name}`,
+      email: `${email}`
+    };
+    const configObj = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify(formData)
+    }
+    return fetch('http://localhost:3000/users', configObj)
+    .then(resp => resp.json())
+    .then(obj => document.body.append(obj.id))
+    .catch(function(error) {
+      alert('Unauthorized Access');
+      document.body.append(error.message)
+    })
+  }

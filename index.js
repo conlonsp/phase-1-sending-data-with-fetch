c5dcb7c6a33131685d1d1f1bb4e992d3eeefa066
samuelconlon <conlonsp@yahoo.com>
2025-01-31 09:01:54 -0500

    commit tree c0453c0656439109974bf60353ee59cf2148de54

--- index.js
+++ index.js
@@ -1,1 +1,106 @@
 // Add your code here
+function submitData(name, email) {
+  const formData = {
+    name: name,
+    email: email
+  }
+
+  const configObj = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify(formData)
+  }
+
+  return fetch('http://localhost:3000/users', configObj)
+  .then(res => res.json())
+  .then(data => document.body.append(data.id))
+  .catch(error => {
+    alert("Unauthorized")
+    document.body.append(error.message)
+  })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const formData = {
+//   name: '',
+//   email: ''
+// }
+
+// const configObj = {
+//   method: 'POST',
+//   headers: {
+//     'Content-Type': 'application/json',
+//     'Accept': 'application/json',
+//   },
+//   body: JSON.stringify(formData),
+// }
+
+// function submitData(name, email) {
+//   const formData = {
+//     name: `${name}`,
+//     email: `${email}`
+//   };
+//   const configObj = {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//       'Accept': 'application/json'
+//     },
+//     body: JSON.stringify(formData)
+//   }
+//   return fetch('http://localhost:3000/users', configObj)
+//   .then(resp => resp.json())
+//   .then(obj => document.body.append(obj.id))
+//   .catch(function(error) {
+//     alert('Unauthorized Access');
+//     document.body.append(error.message)
+//   })
+// }
